6aa436870e82b7c61c7b8e49489bfffa51fe3599
nickGangalo <33544769+nickGangalo@users.noreply.github.com>
2021-06-25 10:55:20 +0200

    Simple ratings view
    My first contribution to GitHub :)
    This will give you a simple way to get an Integer between 0-5 to a State variable. If you want more steps in the rating, just change the upper limit in the ForEach.

---
+++ RatingsView.swift
@@ -0,0 +1,32 @@
+struct RatingsView: View {
+    
+    @Binding var rating: Int    // Sets the rating to a state variable.
+    @Binding var starSize: CGFloat // The size of the stars.
+    @Binding var disabled: Bool // If set to 'true' the rating is not editable so you can use the view to just display results.
+    var starColor: Color // Sets the fill color of the stars.
+    
+    var body: some View {
+        HStack {
+            Image(systemName: rating != 0 ? "star.slash" : "star.slash.fill" )
+                .font(.system(size: starSize))
+                .foregroundColor(starColor)
+                .onTapGesture {
+                    rating = 0
+                }.disabled(disabled)
+            
+            ForEach(1..<6) { idx in
+                ratingButton(idx: idx)
+            }
+        }
+    }
+    
+    func ratingButton(idx: Int) -> some View {
+        
+        Image(systemName: idx <= rating ? "star.fill" : "star")
+            .font(.system(size: starSize))
+            .foregroundColor(starColor)
+            .onTapGesture {
+                rating = idx
+            }.disabled(disabled)
+    }
+}
